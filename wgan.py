64bca09e85d869bc0dc13c7dec8dfc3da2088d64
dodoseung <sok7819@gmail.com>
2023-07-21 15:33:45 +0900

    wgan

---
+++ wgan.py
@@ -0,0 +1,106 @@
+import torch.nn.functional as F
+import torch.nn as nn
+
+# Generator of Ganerative Adversarial Network
+class WGAN(nn.Module):
+    def __init__(self, gen_latent_z=100, gen_init_layer=[7,7,64], gen_conv_trans=[2,2,1,1], gen_conv_filters=[128,64,64,1],
+                 gen_conv_kernels=[5,5,5,5], gen_conv_strides=[1,1,1,1], gen_conv_pads=[2,2,2,2], gen_dropout_rate=0.1,
+                 crt_input_img=[28,28,1], crt_conv_filters=[64,64,128,128], crt_conv_kernels=[5,5,5,5], 
+                 crt_conv_strides=[2,2,2,1], crt_conv_pads=[2,2,2,2], crt_dropout_rate=0.4):
+        super(WGAN, self).__init__()
+        
+        # Set the generator and discriminator
+        self.G = Generator(gen_latent_z, gen_init_layer, gen_conv_trans, gen_conv_filters, gen_conv_kernels, gen_conv_strides, gen_conv_pads, gen_dropout_rate)
+        self.C = Critic(crt_input_img, crt_conv_filters, crt_conv_kernels, crt_conv_strides, crt_conv_pads, crt_dropout_rate)
+
+# Generator of GAN
+class Generator(nn.Module):
+    def __init__(self, latent_z, init_layer, conv_trans, conv_filters, conv_kernels, conv_strides, conv_pads, dropout_rate):
+        super(Generator, self).__init__()
+        # Initiation
+        self.init_layer = init_layer
+        self.latent_z = latent_z
+        self.conv_trans = conv_trans
+        self.conv_filters = conv_filters
+        self.conv_kernels = conv_kernels
+        self.conv_strides = conv_strides
+        self.conv_pads = conv_pads
+        self.dropout_rate = dropout_rate
+        
+        # Append filters for the init layer
+        self.num_layers = len(self.conv_filters)
+        self.conv_filters.insert(0, self.init_layer[2])
+        
+        # Fully connected layer
+        self.fc = nn.Linear(self.latent_z, self.init_layer[0] * self.init_layer[1] * self.init_layer[2])
+        self.dropout = nn.Dropout(p=self.dropout_rate)
+    
+        # Convolution layers
+        self.conv = []
+        for i in range(self.num_layers):
+            layer = nn.Sequential(
+                nn.ConvTranspose2d(conv_filters[i], conv_filters[i], kernel_size=self.conv_trans[i], stride=self.conv_trans[i]),
+                nn.Conv2d(conv_filters[i], conv_filters[i+1], kernel_size=self.conv_kernels[i], stride=self.conv_strides[i], padding=self.conv_pads[i]),
+                nn.BatchNorm2d(conv_filters[i+1]),
+                nn.ReLU())
+            self.conv.append(layer)
+        self.conv = nn.ModuleList(self.conv)
+  
+    def forward(self, z):
+        # FCN
+        x = F.relu(self.fc(z))
+        
+        # Reshape and drop out
+        x = x.view(-1, self.init_layer[2], self.init_layer[0], self.init_layer[1])
+        x = self.dropout(x)
+        
+        # CNN
+        for i in range(self.num_layers):
+            x = self.conv[i](x)
+        
+        return x
+    
+# Critic of GAN
+class Critic(nn.Module):
+    def __init__(self, input_img, conv_filters, conv_kernels, conv_strides, conv_pads, dropout_rate):
+        super(Critic, self).__init__()
+        # Initiation
+        self.input_img = input_img
+        self.conv_filters = conv_filters
+        self.conv_kernels = conv_kernels
+        self.conv_strides = conv_strides
+        self.conv_pads = conv_pads
+        self.dropout_rate = dropout_rate
+        
+        # Append filters for the input image
+        self.num_layers = len(self.conv_filters)
+        self.conv_filters.insert(0, self.input_img[2])
+        
+        # Convolution layers
+        self.conv = []
+        for i in range(self.num_layers):
+            layer = nn.Sequential(
+                nn.Conv2d(conv_filters[i], conv_filters[i+1], kernel_size=self.conv_kernels[i], stride=self.conv_strides[i], padding=self.conv_pads[i]),
+                nn.BatchNorm2d(conv_filters[i+1]),
+                nn.ReLU(),
+                nn.Dropout(p=self.dropout_rate))
+            self.conv.append(layer)
+        self.conv = nn.ModuleList(self.conv)
+        
+        # Output layer
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(self.conv_filters[-1], 1)
+        
+    def forward(self, x):
+        # CNN
+        for i in range(self.num_layers):
+            x = self.conv[i](x)
+            
+        # Output layer
+        x = self.pool(x)
+        x = x.squeeze(-1).squeeze(-1)
+        x = self.fc(x)
+        
+        return x
+
+
---
+++ wgan_emnist_image_generation.py
@@ -0,0 +1,192 @@
+from wgan import WGAN
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+from utils import save_model, load_yaml
+
+# Set the configuration
+config = load_yaml("./config/wgan_config.yml")
+
+# Training setting
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.manual_seed(config['data']['seed'])
+if device == 'cuda':
+  torch.cuda.manual_seed_all(config['data']['seed'])
+
+# Set the transform
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize(config['data']['img_size'])])
+
+# Set the training data
+train_data = datasets.EMNIST(config['data']['data_path'],
+                             download=config['data']['download'],
+                             split='mnist',
+                             train=True,
+                             transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data,
+                                           batch_size=config['data']['batch_size'],
+                                           shuffle=config['data']['shuffle'],
+                                           num_workers=config['data']['num_workers'],
+                                           drop_last=config['data']['drop_last'])
+
+# Set the test data
+test_data = datasets.EMNIST(config['data']['data_path'],
+                            download=config['data']['download'],
+                            split='mnist',
+                            train=False,
+                            transform=transform)
+test_loader = torch.utils.data.DataLoader(test_data,
+                                          batch_size=config['data']['batch_size'],
+                                          shuffle=config['data']['shuffle'],
+                                          num_workers=config['data']['num_workers'],
+                                          drop_last=config['data']['drop_last'])
+
+# Check the categories
+print(len(train_data.classes))
+
+# Set the model
+model = WGAN(gen_latent_z=config['model']['gen_latent_z'], gen_init_layer=config['model']['gen_init_layer'],
+            gen_conv_trans=config['model']['gen_conv_trans'], gen_conv_filters=config['model']['gen_conv_filters'],
+            gen_conv_kernels=config['model']['gen_conv_kernels'], gen_conv_strides=config['model']['gen_conv_strides'],
+            gen_conv_pads=config['model']['gen_conv_pads'],gen_dropout_rate=config['model']['gen_dropout_rate'],
+            crt_input_img=config['model']['crt_input_img'], crt_conv_filters=config['model']['crt_conv_filters'],
+            crt_conv_kernels=config['model']['crt_conv_kernels'], crt_conv_strides=config['model']['crt_conv_strides'],
+            crt_conv_pads=config['model']['crt_conv_pads'], crt_dropout_rate=config['model']['crt_dropout_rate']).to(device)
+
+print(model, device)
+
+# Set the criterion and optimizer
+g_optimizer = optim.RMSprop(model.G.parameters(), lr=config['train']['lr'])
+d_optimizer = optim.RMSprop(model.C.parameters(), lr=config['train']['lr'])
+criterion = nn.BCELoss()
+
+# Set values
+batch_size = config['data']['batch_size']
+z_latent = config['model']['gen_latent_z']
+gen_iteration = config ['train']['gen_iteration']
+crt_clip_value = config['train']['crt_clip_value']
+
+# Training
+def train(epoch, train_loader, g_optimizer, d_optimizer):
+  model.train()
+  g_train_loss = 0.0
+  d_train_loss = 0.0
+  train_num = 0
+  
+  for i, data in enumerate(train_loader, 0):
+    # Critic
+    # get the inputs; data is a list of [inputs, labels]
+    real_img, _ = data
+    
+    # Transfer data to device
+    real_img = real_img.to(device)
+    real_score = model.C(real_img)
+
+    # Generate generated image
+    z = 2 * torch.rand(batch_size, z_latent, device=device) - 1
+    fake_img = model.G(z)
+    fake_score = model.C(fake_img)
+    
+    # Loss for the critic with EM distance
+    d_loss = fake_score.mean() - real_score.mean()
+    
+    # Training for the critic
+    d_optimizer.zero_grad()
+    d_loss.backward()
+    d_optimizer.step()
+    
+    # Clip weights of discriminator
+    for p in model.C.parameters():
+      p.data.clamp_(-crt_clip_value, crt_clip_value)
+    
+    # Generator
+    if i // gen_iteration == 0:
+      # Get the fake images and scores
+      z = 2 * torch.rand(batch_size, z_latent, device=device) - 1
+      fake_img = model.G(z)
+      fake_score = model.C(fake_img)
+
+      # Training for the generator
+      g_loss = - fake_score.mean()
+      g_optimizer.zero_grad()
+      g_loss.backward()
+      g_optimizer.step()
+      
+      # loss
+      g_train_loss += g_loss.item()
+
+    # loss
+    d_train_loss += d_loss.item()
+    train_num += real_img.size(0)
+    
+    if i % config['others']['log_period'] == 0 and i != 0:
+      print(f'[{epoch}, {i}]\t Train loss: (G){g_train_loss / train_num:.10f}, (D){d_train_loss / train_num:.10f}')
+  
+  # Average loss
+  d_train_loss /= train_num
+  
+  return d_train_loss
+
+# Test
+def valid(test_loader):
+  model.eval()
+  g_test_loss = 0.0
+  d_test_loss = 0.0
+  test_loss = 0.0
+  test_num = 0
+
+  for _, data in enumerate(test_loader, 0):
+    # Critic
+    # get the inputs; data is a list of [inputs, labels]
+    real_img, _ = data
+    
+    # Transfer data to device
+    real_img = real_img.to(device)
+    real_score = model.C(real_img)
+
+    # Generate generated image
+    z = 2 * torch.rand(batch_size, z_latent, device=device) - 1
+    fake_img = model.G(z)
+    fake_score = model.C(fake_img)
+    
+    # Loss for the critic with EM distance
+    d_loss = fake_score.mean() - real_score.mean()
+    
+    # Generator
+    # Get the fake images and scores
+    z = 2 * torch.rand(batch_size, z_latent, device=device) - 1
+    fake_img = model.G(z)
+    fake_score = model.C(fake_img)
+
+    # Training for the generator
+    g_loss = - fake_score.mean()
+    
+    # loss
+    g_test_loss += g_loss.item()
+    d_test_loss += d_loss.item()
+    test_num += real_img.size(0)
+  
+  # Test accuracy
+  test_accuracy = test_loss / test_num
+  
+  return test_accuracy
+
+# Main
+if __name__ == '__main__':
+  for epoch in range(config['train']['epochs']):  # loop over the dataset multiple times
+    # Training
+    train_loss = train(epoch, train_loader, g_optimizer, d_optimizer)
+    
+    # Validation
+    test_accuracy = valid(test_loader)
+    
+    # Print the log
+    print(f'Epoch: {epoch}\t Train loss: {train_loss:.10f}\t Valid accuracy: {test_accuracy:.10f}')
+    
+    # Save the model
+    save_model(model_name=config['save']['model_name'], epoch=epoch, model=model, optimizer=g_optimizer, loss=train_loss, config=config)
+    
